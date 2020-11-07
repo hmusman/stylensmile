13131e337291aca42f045ade89ef2a599e0b6421
Mubeen33 <mubeenbaig33@gmail.com>
2020-11-07 17:17:58 +0500

    Mubeen

--- resources/views/subcategories/feed_categories.php
+++ resources/views/subcategories/feed_categories.php
@@ -31,8 +31,7 @@
                 <tr>
                     <th>#</th>
                     <th>{{translate('Subcategory')}}</th>
-                    <th>{{translate('Category')}}</th>
-                    <th width="10%">{{translate('Options')}}</th>
+                    <th>{{translate('Map Category')}}</th>
                 </tr>
             </thead>
             <tbody>
@@ -42,16 +41,19 @@
                             <td>{{ ($key+1) + ($subcategories->currentPage() - 1)*$subcategories->perPage() }}</td>
                             <td>{{__($subcategory->name)}}</td>
                             <td>{{$subcategory->category->name}}</td>
-                            <td>
-                                <div class="btn-group dropdown">
-                                    <button class="btn btn-primary dropdown-toggle dropdown-toggle-icon" data-toggle="dropdown" type="button">
-                                        {{translate('Actions')}} <i class="dropdown-caret"></i>
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-right">
-                                        <li><a href="{{route('subcategories.edit', encrypt($subcategory->id))}}">{{translate('Edit')}}</a></li>
-                                        <li><a onclick="confirm_modal('{{route('subcategories.destroy', $subcategory->id)}}');">{{translate('Delete')}}</a></li>
-                                    </ul>
-                                </div>
+                            <td colspan="3">
+
+                                <select class="form-control" style="background: green;color: #fff" id="subcat_id{{$subcategory->id }}" onchange="assignCategory(this,{{ $subcategory->id }})">
+                                        <option value="0">none</option>
+                                   @foreach($feedCategories as $feedcategory)
+                                    @if($subcategory->feedcat_id == $feedcategory->feed_id)
+
+                                            <option value="{{ $feedcategory->feed_id }}" selected>{{ $feedcategory->feed_id }}-{{ $feedcategory->name }}</option>
+                                    @else
+                                            <option value="{{ $feedcategory->feed_id }}">{{ $feedcategory->feed_id }}-{{ $feedcategory->name }}</option>
+                                    @endif    
+                                   @endforeach     
+                                </select>    
                             </td>
                         </tr>
                     @endif
