4fe47def36fe584a1f4a346dc2c0f463f5abc76a
Mubeen33 <mubeenbaig33@gmail.com>
2020-12-31 16:28:16 +0500

    tags google

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -85,6 +85,15 @@
         gtag('js', new Date());
         gtag('config', '{{ env('TRACKING_ID') }}');
     </script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H2CTE48QML"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-H2CTE48QML');
+        </script>
 @endif
 
 @if (\App\BusinessSetting::where('type', 'facebook_pixel')->first()->value == 1)
